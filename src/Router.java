8086be2183ea13ef58c2eab1dd25080734db0b19
Alemas <mateusreckziegel@gmail.com>
2018-06-04 16:20:07 -0300

    Fixed router sending packets straight to other network hosts instead of respective router bug

--- src/Router.java
+++ src/Router.java
@@ -19,19 +19,20 @@ public class Router extends HostBase {
 //		for (String s : message) { printMessage(s); }
 		String origin = message[0];
 		String destination = message[2];
-		String destPort = message[3];
+		int destPort = Integer.parseInt(message[3]);
 		
 //		printMessage(destination);
 //		printMessage(localAddress.toString());
-//		if (origin.hashCode() == localAddress.toString().hashCode()) {
-//			printMessage("Received data from inside the network");
-//		} else {
-//			printMessage("Received data from outside the network");
-//		}
+		if (origin.hashCode() == localAddress.toString().hashCode()) {
+			printMessage("Received data from inside the network");
+			destPort = LocalNetwork.RouterPort;
+		} else {
+			printMessage("Received data from outside the network");
+		}
 		printMessage("Redirecting received packet...");
 		printMessageHeader(message);
 		try {
-			sendTo(new String(packet.getData()), InetAddress.getByName(destination.replace("/", "")), Integer.parseInt(destPort));
+			sendTo(new String(packet.getData()), InetAddress.getByName(destination.replace("/", "")), destPort);
 		} catch (Exception e) {
 			printMessage(e.toString());
 		}
